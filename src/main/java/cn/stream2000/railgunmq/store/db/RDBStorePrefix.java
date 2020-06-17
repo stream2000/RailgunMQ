fa098f736c13cb3342c74ab29898816ae65601f7
stream2000 <stream2000@139.com>
2020-06-17 21:23:23 +0800

    migrate rdb package from jmqtt

---
+++ src/main/java/cn/stream2000/railgunmq/store/db/RDBStorePrefix.java
@@ -0,0 +1,7 @@
+package cn.stream2000.railgunmq.store.db;
+
+public interface RDBStorePrefix {
+
+    String PERSISTENCE_MESSAGE = "persistenceMessage-";
+    String UN_ACK_MESSAGE = "unAckMessage-";
+}
