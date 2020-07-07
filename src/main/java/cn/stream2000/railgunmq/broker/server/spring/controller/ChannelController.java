9b8952491bd44fbb4d01a47e298692c6007942e0
stream2000 <stream2000@139.com>
2020-07-08 06:24:02 +0800

    feat: delete connection

--- src/main/java/cn/stream2000/railgunmq/broker/server/spring/controller/ChannelController.java
+++ src/main/java/cn/stream2000/railgunmq/broker/server/spring/controller/ChannelController.java
@@ -1,33 +1,47 @@
 package cn.stream2000.railgunmq.broker.server.spring.controller;
 
+import cn.stream2000.railgunmq.broker.subscribe.Topic;
+import cn.stream2000.railgunmq.broker.subscribe.TopicManager;
 import cn.stream2000.railgunmq.core.Connection;
+import cn.stream2000.railgunmq.core.Connection.ConnectionRole;
 import cn.stream2000.railgunmq.core.ConnectionMap;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class ChannelController {
 
     @GetMapping("/channel/getAll")
     @ResponseBody
-    public List<Connection> getAllChannel() {return ConnectionMap.getAll();}
+    public List<Connection> getAllChannel() {
+        return ConnectionMap.getAll();
+    }
 
     @GetMapping("/channel/get")
     @ResponseBody
     public Connection getChannel(String key) {
-        return  ConnectionMap.getConnection(key);
+        return ConnectionMap.getConnection(key);
     }
 
     @GetMapping("/channel/delete")
     @ResponseBody
     public void deleteChannel(String key) {
-        ConnectionMap.getConnection(key).getChannel().disconnect();
-        ConnectionMap.deleteConnection(key);
-
+        Connection conn = ConnectionMap.getConnection(key);
+        if (conn == null) {
+            return;
+        }
+        ConnectionMap.deleteConnection(conn.getChannelId());
+        conn.getChannel().disconnect();
+        if (conn.getRole().equals(ConnectionRole.Consumer)) {
+            String topicName = conn.getTopic();
+            assert topicName != null;
+            Topic topic = TopicManager.getTopic(topicName);
+            if (topic != null) {
+                topic.removeSubscription(conn.getChannelId());
+            }
+        }
     }
-
 
 }
