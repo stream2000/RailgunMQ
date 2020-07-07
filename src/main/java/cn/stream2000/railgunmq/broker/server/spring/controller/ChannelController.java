dd386174dabf0af565a7877c485d92d0180f2e48
HarviMa <943941437@qq.com>
2020-07-07 13:04:58 +0800

    merge changes

--- src/main/java/cn/stream2000/railgunmq/broker/server/spring/controller/ChannelController.java
+++ src/main/java/cn/stream2000/railgunmq/broker/server/spring/controller/ChannelController.java
@@ -3,6 +3,7 @@ package cn.stream2000.railgunmq.broker.server.spring.controller;
 import cn.stream2000.railgunmq.broker.subscribe.Topic;
 import cn.stream2000.railgunmq.broker.subscribe.TopicManager;
 import cn.stream2000.railgunmq.core.ChannelMap;
+import cn.stream2000.railgunmq.core.Connection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class ChannelController {
     @GetMapping("/channel/getAll")
     @ResponseBody
     public List<String> getAllChannels() {
-        Map<String, Channel> channelMap = ChannelMap.getAllChannels();
+        Map<String, Connection> channelMap = ChannelMap.getAllChannels();
         List<String> channelName = new ArrayList<String>();
         for(String key: channelMap.keySet()){
             channelName.add(key);
@@ -27,14 +28,14 @@ public class ChannelController {
 
     @GetMapping("/channel/get")
     @ResponseBody
-    public Channel getChannel(String key) {
+    public Connection getChannel(String key) {
         return  ChannelMap.getChannel(key);
     }
 
     @GetMapping("/channel/delete")
     @ResponseBody
     public void deleteChannel(String key) {
-        ChannelMap.getChannel(key).close();
+        ChannelMap.getChannel(key).getChannel().close();
         ChannelMap.deleteChannel(key);
 
     }
