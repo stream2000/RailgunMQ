b1d5690b583f500ebfae0d820a8d82701b9e44a1
Zhangxin <2403728388@qq.com>
2020-07-07 20:42:30 +0800

    7

--- src/main/java/cn/stream2000/railgunmq/producer/RailgunMQConnection.java
+++ src/main/java/cn/stream2000/railgunmq/producer/RailgunMQConnection.java
@@ -21,6 +21,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @ChannelHandler.Sharable
 public class RailgunMQConnection {
     private final String host;
@@ -28,6 +30,7 @@ public class RailgunMQConnection {
     private Connection connection;
     private BlockingQueue<ProducerMessage.PubMessageAck> blockingQueue;
     private String channelId;
+    private AtomicInteger atomicInteger;//自增id生成器
     //在初始化时指明IP和端口
     public RailgunMQConnection(String host, int port) throws InterruptedException {
         this.host = host;
@@ -42,9 +45,8 @@ public class RailgunMQConnection {
         connection=new Connection();
         connection.setChannel(channel);
         connection.setConnectionName(channel.id().asLongText());
-
-
         blockingQueue=new LinkedBlockingDeque<ProducerMessage.PubMessageAck>();
+        atomicInteger=new AtomicInteger();
     }
     public RailgunMQConnection(String host, int port, String connectionName) throws InterruptedException {
         this.host = host;
@@ -59,6 +61,7 @@ public class RailgunMQConnection {
         connection.setConnectionName(connectionName);
         connection.setChannel(channel);
         blockingQueue=new LinkedBlockingDeque<ProducerMessage.PubMessageAck>();
+        atomicInteger=new AtomicInteger();
         SetChannelName(connectionName);
     }
 
@@ -72,6 +75,10 @@ public class RailgunMQConnection {
         connection.getChannel().writeAndFlush(setChannelName);
     }
 
+    public String  getChannelId()
+    {
+        return this.channelId;
+    }
 
     public void Disconnect()
     {
@@ -120,53 +127,58 @@ public class RailgunMQConnection {
 
     //这里的send不应该返回空值，先看吧
     //暂时不考虑消息发送策略
-    public void Publish(String topic,String message)
+    public int Publish(String topic,String message)
     {
         try {
-            int uuid = UUID.randomUUID().hashCode();
+            int id= atomicInteger.getAndIncrement();
             ProducerMessage.PubMessageRequest request =
                     ProducerMessage.PubMessageRequest.newBuilder().setChannelId(channelId)
-                            .setLetterId(uuid)
+                            .setLetterId(id)
                             .setType(ProducerMessage.PubMessageRequest.payload_type.Text)
                             .setData(ByteString.copyFromUtf8(message))
                             .setTopic(topic).build();
             connection.getChannel().writeAndFlush(request);
+            return id;
         } catch (Exception e) {
             e.printStackTrace();
+            return -1;
         }
     }
 
 
-    public void Publish(String topic, byte[] bytes)
+    public int Publish(String topic, byte[] bytes)
     {
         try {
-            int uuid = UUID.randomUUID().hashCode();
+            int id= atomicInteger.getAndIncrement();
             ProducerMessage.PubMessageRequest request =
                     ProducerMessage.PubMessageRequest.newBuilder().setChannelId(channelId)
-                            .setLetterId(uuid)
+                            .setLetterId(id)
                             .setType(ProducerMessage.PubMessageRequest.payload_type.Binary)
                             .setData(ByteString.copyFrom(bytes))
                             .setTopic(topic).build();
             connection.getChannel().writeAndFlush(request);
-
+            return id;
         } catch (Exception e) {
             e.printStackTrace();
+            return -1;
         }
     }
 
-    public void Publish(String topic, int value)
+    public int Publish(String topic, int value)
     {
         try {
-            int uuid = UUID.randomUUID().hashCode();
+            int id= atomicInteger.getAndIncrement();
             ProducerMessage.PubMessageRequest request =
                     ProducerMessage.PubMessageRequest.newBuilder().setChannelId(channelId)
-                            .setLetterId(uuid)
+                            .setLetterId(id)
                             .setType(ProducerMessage.PubMessageRequest.payload_type.Integer)
                             .setData(ByteString.copyFromUtf8(Integer.toString(value)))
                             .setTopic(topic).build();
            connection.getChannel().writeAndFlush(request);
+           return id;
         } catch (Exception e) {
             e.printStackTrace();
+            return -1;
         }
     }
     //处理返回的函数，在ClientInitializer中注册
