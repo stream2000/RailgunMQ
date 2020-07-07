63de5ae4eb396b0a517e8ac8bffa385cebc5c7f8
stream2000 <stream2000@139.com>
2020-07-08 00:08:35 +0800

    fixed: no more sending back useless response

--- src/main/java/cn/stream2000/railgunmq/broker/BrokerMessageHandler.java
+++ src/main/java/cn/stream2000/railgunmq/broker/BrokerMessageHandler.java
@@ -2,7 +2,6 @@ package cn.stream2000.railgunmq.broker;
 
 import cn.stream2000.railgunmq.broker.strategy.CommonStrategy.DisconnectStrategy;
 import cn.stream2000.railgunmq.core.ConnectionMap;
-import cn.stream2000.railgunmq.core.ProducerMessage;
 import cn.stream2000.railgunmq.netty.MessageEventWrapper;
 import cn.stream2000.railgunmq.netty.codec.MessageStrategyContext;
 import io.netty.channel.ChannelHandler;
@@ -13,14 +12,6 @@ public class BrokerMessageHandler extends MessageEventWrapper<Object> {
 
     public BrokerMessageHandler() {
         super.setWrapper(this);
-    }
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ProducerMessage.CreateChannelResponse response =
-            ProducerMessage.CreateChannelResponse.newBuilder()
-                .setChannelId(ctx.channel().id().asLongText()).build();
-        ctx.writeAndFlush(response);
     }
 
     @Override
